ce5f8a19e8bc4622bc99d89d285ca449a76db04f
ihssan benlabsir <benlabsirihssan@gmail.com>
2024-02-01 22:38:26 +0100

    Create lcd_video_youtub_curseur.ino

---
+++ code/lcd_video_youtub_curseur/lcd_video_youtub_curseur.ino
@@ -0,0 +1,31 @@
+#include <LiquidCrystal.h>
+#include <Keypad.h>
+LiquidCrystal lcd(13, 12, 11, 10, 9, 8);
+
+const byte ROWS = 4; //four rows
+const byte COLS = 4; //four columns
+char keys[ROWS][COLS] = {
+  {'7','8','9','/'},
+  {'4','5','6','*'},
+  {'1','2','3','-'},
+  {' ','0','=','+'}
+};
+byte rowPins[ROWS] = {7, 6, 5, 4};
+byte colPins[COLS] = {3, 2, 1,0};
+ 
+Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+
+void setup(){
+  lcd.begin(16,2);
+}
+  
+void loop(){
+  char key = keypad.getKey();
+  
+  if (key){
+    lcd.print(key);
+    lcd.cursor();//affichr le curseur
+    lcd.blink();// clignetement de lcd
+    lcd.setCursor(0,1);
+  }
+}
